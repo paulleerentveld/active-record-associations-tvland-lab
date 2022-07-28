3301b786fc849d5a947b9e1cbdddda182bb842dc
paulleerentveld <paul@veldnet.com.au>
2022-07-28 18:52:12 +1000

    lab done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+
+    def actors_list
+        characters.map do |character|
+            character.actor.full_name
+        end
+    end
   
 end
